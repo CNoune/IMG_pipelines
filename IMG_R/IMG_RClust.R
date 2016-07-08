6208b0730b34524aff31bb80b0740360cbe87a0d
CNoune <cnoune@gmail.com>
2016-07-08 14:38:03 +1000

    IMG_RClust - version 2.0
    Added functions to accept user input.

--- IMG_R/IMG_RClust.R
+++ IMG_R/IMG_RClust.R
@@ -1,15 +1,18 @@
-# v1.5
+# IMG_RClust- version 2.0
+# Copyright (c) 2016 Christopher Noune
 
 #Install Packages - Function found on (http://stackoverflow.com/questions/9341635/check-for-installed-packages-before-running-install-packages)
-packages<-function(x){
+packages_check<-function(x){
   x<-as.character(match.call()[[2]])
   if (!require(x,character.only=TRUE)){
     install.packages(pkgs=x,repos="http://cran.r-project.org")
     require(x,character.only=TRUE)
   }
 }
-packages(mclust)
-packages(ape)
+packages_check(mclust)
+packages_check(ape)
+packages_check(tcltk)
+packages_check(tcltk2)
 
 #Determine Clusters
 library(ape)
@@ -18,23 +21,63 @@ Alignment_distance <- dist.dna(Alignment) #creates distance matrix
 standard_distance <- scale(Alignment_distance) #scales distance matrix columns
 cluster_amounts <- (nrow(standard_distance)-1)*sum(apply(standard_distance,2,var)) #manually determine cluster amounts
 for (i in 2:15) cluster_amounts[i] <- sum(kmeans(standard_distance, centers=i)$withinss) #cluster amounts up to 15 clusters - http://www.statmethods.net/advstats/cluster.html
-plot(1:15, cluster_amounts, type="b", xlab="Number of Clusters", ylab="Within groups sum of squares") #plotting clusters - http://www.statmethods.net/advstats/cluster.html
+#export the plot
+plot(1:15, cluster_amounts, type="b", main = "Manual K Determination", xlab="Number of Clusters", ylab="Within groups sum of squares") #plotting clusters - http://www.statmethods.net/advstats/cluster.html
 #If error "more centres than distinct data points" is produced, 
 #skip to validation step and use the cluster value mclust determines in the kmeans clustering step.
 
-#validation
+#validation with MCLUST
 library(mclust)
 validation <- Mclust(standard_distance)
 summary(validation)
-plot(validation) #Select the bayesian information criterion (BIC) result and export it.
+
+#Input K Value from validation step
+ask_K <- function(){
+  require(tcltk2)
+  tt <- tktoplevel()
+  k <- tclVar(0)
+  
+  K_frame <- tkframe(tt)
+  tkpack(K_frame, side='top')
+  tkpack(tklabel(K_frame, text='Please Specify the K value: '), side='left')
+  tkpack(tkentry(K_frame, textvariable=k), side='left')
+  
+  tkpack(tkbutton(tt, text='continue', command=function() tkdestroy(tt)),
+         side='right', anchor='s')
+  
+  tkwait.window(tt)
+  return( c(k=as.numeric(tclvalue(k))))
+}
+
+k <- ask_K()
 
 #Kmeans clustering - iter.max is bootstrapping the results
-fitted_model <- kmeans(standard_distance, "put the cluster number here from MCLUST/Manual calculation", iter.max = 100) #creating a fitted model
+fitted_model <- kmeans(standard_distance, k, iter.max = 100) #creating a fitted model
 cluster_means <- aggregate(standard_distance,by=list(fitted_model$cluster),FUN=mean) #determines clustering means
 final_results <- data.frame(standard_distance, fitted_model$cluster) #creates the final results with clusters
-
 #If Mclust clusters and kmeans clusters results agree, accept the data. 
 #If not, repeat kmeans clustering with more clusters.
 #I.E. the fitted_model cluster selection and mclust cluster determination should be the same.
+
 #Output the results as .csv.
-write.csv(final_results, file = "Clusters.csv")
+ask_fileOUT <- function(){
+  require(tcltk2)
+  tt <- tktoplevel()
+  file_name <- tclVar(0)
+  
+  File_frame <- tkframe(tt)
+  tkpack(File_frame, side='top')
+  tkpack(tklabel(File_frame, text='Please Input Filename followed by .csv: '), side='left')
+  tkpack(tkentry(File_frame, textvariable=file_name), side='left')
+  
+  tkpack(tkbutton(tt, text='continue', command=function() tkdestroy(tt)),
+         side='right', anchor='s')
+  
+  tkwait.window(tt)
+  return( c(file_name=as.character(tclvalue(file_name))))
+}
+
+file_name <- ask_fileOUT()
+write.csv(final_results, file = file_name)
+#Export bayesian information criterion (BIC) result
+plot(validation$BIC)
