c5d9c8f740214707eba69fc588eae3690aad1471
CNoune <cnoune@gmail.com>
2017-01-06 08:31:25 +1000

    Delete Set_wrk_dir.sh

--- IMG_GAP/IMG_GAP_Back-ends/Set_wrk_dir.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Press [ENTER] to Select Working Directory"
-read enter
-wrk_dir="`zenity --file-selection --directory`"
-IMG_GAP="$PWD/IMG_pipelines/IMG_GAP/IMG_GAP-build_13.sh"
-(cd $wrk_dir && bash $IMG_GAP)
-
