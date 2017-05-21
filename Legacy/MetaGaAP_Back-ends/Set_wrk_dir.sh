a7a34a226fc02fb5a8c6584179a33a9b4eb9b6dc
Christopher Noune <cnoune@gmail.com>
2017-05-21 16:34:11 +1000

    Update Set_wrk_dir.sh

--- Legacy/MetaGaAP_Back-ends/Set_wrk_dir.sh
+++ Legacy/MetaGaAP_Back-ends/Set_wrk_dir.sh
@@ -2,5 +2,5 @@
 echo "Press [ENTER] to Select Working Directory"
 read enter
 wrk_dir="`zenity --file-selection --directory`"
-MetaGaAP="$PWD/IMG_pipelines/MetaGaAP/MetaGaAP.sh"
+MetaGaAP="$PWD/IMG_pipelines/Legacy/MetaGaAP-Legacy.sh"
 (cd $wrk_dir && bash $MetaGaAP)
